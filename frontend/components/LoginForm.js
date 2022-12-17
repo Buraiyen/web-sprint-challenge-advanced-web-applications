add69c22ca124f17d68a138e857d3f23039ccba3
Brian Nguyen <brian.edison.nguyen@gmail.com>
2022-12-17 12:26:49 -0800

    feat: button disabled functionality

--- frontend/components/LoginForm.js
+++ frontend/components/LoginForm.js
@@ -1,54 +1,61 @@
-import React, { useState } from 'react'
-import PT from 'prop-types'
+import React, { useState } from 'react';
+import PT from 'prop-types';
 
 const initialFormValues = {
   username: '',
   password: '',
-}
+};
 export default function LoginForm(props) {
-  const [values, setValues] = useState(initialFormValues)
+  const [values, setValues] = useState(initialFormValues);
+  const [buttonDisabled, setButtonDisabled] = useState(true);
   // ✨ where are my props? Destructure them here
 
-  const onChange = evt => {
-    const { id, value } = evt.target
-    setValues({ ...values, [id]: value })
-  }
+  const inputChangeHandler = (evt) => {
+    const { id, value } = evt.target;
+    setValues({ ...values, [id]: value });
+    setButtonDisabled(
+      !(values.username.trimEnd() >= 3 && values.password.trimEnd() >= 8)
+    );
+  };
 
-  const onSubmit = evt => {
-    evt.preventDefault()
+  const onSubmit = (evt) => {
+    evt.preventDefault();
     // ✨ implement
-  }
+  };
 
   const isDisabled = () => {
     // ✨ implement
     // Trimmed username must be >= 3, and
     // trimmed password must be >= 8 for
     // the button to become enabled
-  }
+    return;
+  };
 
   return (
-    <form id="loginForm" onSubmit={onSubmit}>
+    <form id='loginForm' onSubmit={onSubmit}>
       <h2>Login</h2>
       <input
         maxLength={20}
         value={values.username}
-        onChange={onChange}
-        placeholder="Enter username"
-        id="username"
+        onChange={inputChangeHandler}
+        placeholder='Enter username'
+        id='username'
       />
       <input
         maxLength={20}
         value={values.password}
-        onChange={onChange}
-        placeholder="Enter password"
-        id="password"
+        onChange={inputChangeHandler}
+        placeholder='Enter password'
+        id='password'
       />
-      <button disabled={isDisabled()} id="submitCredentials">Submit credentials</button>
+      <button disabled={buttonDisabled} id='submitCredentials'>
+        Submit credentials
+      </button>
     </form>
-  )
+  );
 }
 
 // 🔥 No touchy: LoginForm expects the following props exactly:
 LoginForm.propTypes = {
   login: PT.func.isRequired,
-}
+};
